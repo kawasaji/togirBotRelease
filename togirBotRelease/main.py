07e9830bac9a3ba79a730e498d2cc79f2fc5404f
kawasaji <jimmyhuseynli@gmail.com>
2022-07-08 21:12:07 +0400

    Update main.py

--- togirBotRelease/main.py
+++ togirBotRelease/main.py
@@ -1,23 +1,8 @@
-import aiogram.utils.exceptions
-from aiogram import Bot, Dispatcher, executor, types
-from aiogram.types import ShippingOption, ShippingQuery, LabeledPrice, PreCheckoutQuery
-from aiogram.types.message import ContentType
-import random, time
-from createImage import createImage
 import sqlite3
 import asyncio
 from messges import *
-import re
-import os
 from union import BotDB
-from aiogram import types
 
-
-
-PRICES = [
-    LabeledPrice(label='Ноутбук', amount=1000),
-    LabeledPrice(label='Прочная упаковка', amount=1000)
-]
 
 conn = sqlite3.connect('orders.db')
 cur = conn.cursor()
@@ -29,14 +14,8 @@ bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
 BotDB = BotDB('chatId.db')
 
-
-
-
 loop = asyncio.new_event_loop()
 if __name__ == '__main__':
     from handlers import dp
     executor.start_polling(dp, skip_updates=True)
     # executor.start_polling(dp, loop=loop)
-
-# if __name__ == '__main__':
-#
